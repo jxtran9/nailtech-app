b6455bb944f6f569d43d5b572b22dbd5efa2ea42
jxtran9 <justin.tran12@yahoo.com>
2026-05-27 00:01:57 -0700

    Use Railway DATABASE_URL

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:ChickenNugget2_@localhost/nailtech_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
 
 
